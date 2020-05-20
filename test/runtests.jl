101981b465414ef514585a495db1b00b195592a8
Nelson Lopes <nelson.lopes@tecnico.ulisboa.pt>
2020-05-21 00:09:18 +0100

    comm9
    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,15 @@
-#using Pkg
 
-#Pkg.add(xim)
-
-#using xim
+using xim
 using Test
+
+xim_haircut_testsum(1,2)
+xim_basics_testsum(3,4)
 
 @testset "xim.jl" begin
     # Write your own tests here.
 
-#xim_haircut_testsum(1,2)
+xim_haircut_testsum(1,2)
 
-println("hi")
+#println("hi")
 
 end
