1a290a8dc987b4a349b2b186f4e5af660cc4a3c0
Nelson Lopes <nelson.lopes@tecnico.ulisboa.pt>
2020-05-21 01:23:49 +0100

    com14
    tt

--- src/xim_basics.jl
+++ src/xim_basics.jl
@@ -17,14 +17,15 @@ end
 #xim_basics_testsum(a,b)=a+b
 #*************************************************
 function xim_basics_load_packages()
-  using Pkg;
-  Pkg.add("Images");
-  Pkg.add("ImageView");
-  Pkg.add("TestImages");
-  Pkg.add("FileIO");
-  Pkg.add("Colors");
-  Pkg.add("QuartzImageIO");
+  #using Pkg;
+  #Pkg.add("Images");
+  #Pkg.add("ImageView");
+  #Pkg.add("TestImages");
+  #Pkg.add("FileIO");
+  #Pkg.add("Colors");
+  #Pkg.add("QuartzImageIO");
 
-  using Images, ImageView, TestImages, FileIO, Colors, QuartzImageIO;
+  #using Images, ImageView, TestImages, FileIO, Colors, QuartzImageIO;
+  println("impossible?")
 end
 #*************************************************
